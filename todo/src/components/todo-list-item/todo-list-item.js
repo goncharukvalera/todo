b9a7484f34d574a2ffc57bd1dae3c352997173d3
goncharuk.valera <goncharukvalera@gmail.com>
2019-04-16 16:04:35 +0300

    State (important) + refactoring

--- todo/src/components/todo-list-item/todo-list-item.js
+++ todo/src/components/todo-list-item/todo-list-item.js
@@ -6,7 +6,8 @@ export default class TodoListItem extends React.Component {
     constructor(){
         super();
         this.state = {
-            done: false
+            done: false,
+            important: false
         };
     }
     onLabelClick = () => {
@@ -15,30 +16,31 @@ export default class TodoListItem extends React.Component {
             done: !this.state.done
         });
     };
+    onMarkImportant = () => {
+        this.setState({
+            important: !this.state.important // не обязательно перечислять весь новый state, React подставит только отличия, это очень удобная особенность, котороая позволяет писать более чистый код
+        });
+    };
 
     render() {
-        const {label, important = false} = this.props, // деструктурирование props потому что ф-я render() не принимает параметры
-              {done} = this.state;
-        console.log('done:', done);
+        const {label} = this.props, // деструктурирование props потому что ф-я render() не принимает параметры
+              {done, important} = this.state;
+        console.log('done:', done, '\n', 'important:', important);
         let classNames = 'todo-list-item';
         classNames += done ? ' done' : '';
-        const style = {
-            color: important ? 'steelblue' : 'black',
-            fontWeight: important ? 'bold' : 'normal'
-        };
+        classNames += important ? ' important' : '';
 
         return (
             <span className={classNames}>
               <span
                   className="todo-list-item-label"
-                  // onClick={()=>console.log(`Done: ${label}`)}
-                  onClick={this.onLabelClick}
-                  style={style}>
+                  onClick={this.onLabelClick}>
                 {label}
               </span>
         
               <button type="button"
-                      className="btn btn-outline-success btn-sm float-right">
+                      className="btn btn-outline-success btn-sm float-right"
+                      onClick={this.onMarkImportant}>
                 <i className="fa fa-exclamation"/>
               </button>
         
@@ -50,35 +52,3 @@ export default class TodoListItem extends React.Component {
         );
     }
 }
-
-// Если нет явных причин (компонету не нужно хранить своё состояние (stateless компонент)) использовать компоненты классы - используем компоненты ф-ции
-
-// const TodoListItemFunc = ({label, important = false}) => {
-//
-//     const style = {
-//         color: important ? 'steelblue' : 'black',
-//         fontWeight: important ? 'bold' : 'normal'
-//     };
-//
-//     return (
-//         <span className="todo-list-item">
-//       <span
-//           className="todo-list-item-label"
-//           style={style}>
-//         {label}
-//       </span>
-//
-//       <button type="button"
-//               className="btn btn-outline-success btn-sm float-right">
-//         <i className="fa fa-exclamation"/>
-//       </button>
-//
-//       <button type="button"
-//               className="btn btn-outline-danger btn-sm float-right">
-//         <i className="fa fa-trash-o"/>
-//       </button>
-//     </span>
-//     );
-// };
-//
-// export default TodoListItem;
