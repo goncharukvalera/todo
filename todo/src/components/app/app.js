ae4a4183114c858e127936b526d563d361f39d7f
goncharuk.valera <goncharukvalera@gmail.com>
2019-04-18 12:21:25 +0300

    Delete component. Refactor to Component-class

--- todo/src/components/app/app.js
+++ todo/src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import AppHeader from './../app-header';
 import SearchPanel from './../search-panel/search-panel';
@@ -6,28 +6,36 @@ import TodoList from './../todo-list/todo-list';
 import ItemStatusFilter from './../item-status-filter/item-status-filter';
 import './app.css';
 
-const App = () => {
+export default class App extends Component {
 
-  const todoData = [
-    { label: 'Drink Coffee', important: false, id: 1 },
-    { label: 'Make Awesome App', important: true, id: 2 },
-    { label: 'Have a lunch', important: false, id: 3 }
-  ];
+    constructor(){
+        super();
+        this.state = {
+        todoData: [
+            {label: 'Drink Coffee', important: false, id: 1},
+            {label: 'Make Awesome App', important: true, id: 2},
+            {label: 'Have a lunch', important: false, id: 3}
+        ]
+    };
+    }
+    deleteItem = (id) => {
+        console.log('deleted', id)
+    };
 
-  return (
-    <div className="todo-app">
-      <AppHeader toDo={1} done={3} />
-      <div className="top-panel d-flex">
-        <SearchPanel />
-        <ItemStatusFilter />
-      </div>
-
-      <TodoList 
-          todos={todoData}
-          onDeleted={ (id) => {console.log('deleted', id)}}
-      />
-    </div>
-  );
+    render() {
+        return (
+        <div className="todo-app">
+          <AppHeader toDo={1} done={3} />
+          <div className="top-panel d-flex">
+            <SearchPanel />
+            <ItemStatusFilter />
+          </div>
+        
+          <TodoList 
+              todos={this.state.todoData}
+              onDeleted={ (id) => {this.deleteItem(id)}}
+          />
+        </div>
+        );
+    }
 };
-
-export default App;
