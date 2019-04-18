3489395e15ac8b821191c05d15f1a7a98d5b2976
goncharuk.valera <goncharukvalera@gmail.com>
2019-04-18 13:04:53 +0300

    Delete component. Change state (immutable state!)

--- todo/src/components/app/app.js
+++ todo/src/components/app/app.js
@@ -11,15 +11,29 @@ export default class App extends Component {
     constructor(){
         super();
         this.state = {
-        todoData: [
-            {label: 'Drink Coffee', important: false, id: 1},
-            {label: 'Make Awesome App', important: true, id: 2},
-            {label: 'Have a lunch', important: false, id: 3}
-        ]
-    };
+            todoData: [
+                {label: 'Drink Coffee', important: false, id: 1},
+                {label: 'Make Awesome App', important: true, id: 2},
+                {label: 'Have a lunch', important: false, id: 3}
+            ]
+        };
     }
     deleteItem = (id) => {
-        console.log('deleted', id)
+        this.setState(({todoData}) => { // принимаем как аргумент state и сразу деструктурируем
+            // получаем индекс элемента который удаляем
+            const index = todoData.findIndex((el) => el.id === id);
+            // можно удалить нужный нам элемент из массива методом splice() 
+            // todoData.splice(index, 1)
+            // или просто с помощью delete
+            // delete todoData[index]
+            // но так мы нарушаем один из основных принципов React - мы меняем state напрямую, так делать нельзя! 
+            // Поэтому передадим новый массив состоящий из двух частей (до текущего и после текущего), а потом объединим их
+            const newState = [
+                ... todoData.slice(0, index), // с помощью spread оператора сразу формируем новый массив из частей state
+                ... todoData.slice(index + 1)
+            ];
+            return {todoData: newState}
+        });
     };
 
     render() {
