9f283143932b25ff03e0ca251e250426c47ee3d8
goncharukvalera <goncharukvalera@gmail.com>
2019-04-25 23:13:20 +0300

    Add new item form + styling

--- todo/src/components/item-add-form/item-add-form.js
+++ todo/src/components/item-add-form/item-add-form.js
@@ -2,11 +2,26 @@ import React, {Component} from 'react';
 import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
+    constructor(){
+        super();
+        this.state = {label: ''};
+    }
+    onItemChange = (e) => {
+        // console.log(e.target.value);
+        this.setState({label: e.target.value});
+    };
     render() {
-        return <button
-                  className="add-item btn btn-outline-secondary"
-                  onClick={() => this.props.onItemAdded('New Item')}>
-            Add Item
-        </button>
+        return <form className="item-add-form">
+            <input  type="text"
+                    className="new-item-name form-control"
+                    onChange={this.onItemChange}
+                    placeholder="What needs to be done"
+            />
+            <button
+                      className="add-item btn btn-outline-secondary"
+                      onClick={() => this.props.onItemAdded('New Item')}>
+                Add Item
+            </button>
+        </form>
     }
 }
--- todo/src/components/search-panel/search-panel.js
+++ todo/src/components/search-panel/search-panel.js
@@ -4,7 +4,7 @@ import './search-panel.css';
 
 const SearchPanel = () => {
     const placeholder = 'Type here to search';
-    return <input type="text" placeholder={placeholder} className="search-input" autoComplete="off"/>;
+    return <input type="text" placeholder={placeholder} className="search-input form-control" autoComplete="off"/>;
 };
 
 export default SearchPanel;
