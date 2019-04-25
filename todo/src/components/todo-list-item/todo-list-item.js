d8a7384a41fa0a16598af91fcd9b59f1e9c2dbf2
goncharuk.valera <goncharukvalera@gmail.com>
2019-04-25 10:49:15 +0300

    Data (done, important) refactor

--- todo/src/components/todo-list-item/todo-list-item.js
+++ todo/src/components/todo-list-item/todo-list-item.js
@@ -3,30 +3,8 @@ import React from 'react';
 import './todo-list-item.css';
 
 export default class TodoListItem extends React.Component {
-    constructor(){
-        super();
-        this.state = {
-            done: false,
-            important: false
-        };
-    }
-    onLabelClick = () => {
-        // setState() работает асинхронно и на момент установки нового state текущий state не всегда имеет самую актуальную версию, 
-        // поэтому чтобы избежать тяжело отлавливаемых ошибок нужно немного изменить вызов ф-и setState() с явным указанием state,
-        // тогда React будет рендерить компонент только после реального обновления state
-        this.setState((state) => { 
-            return {done: !state.done}
-        });
-    };
-    onMarkImportant = () => {
-        this.setState(({important}) => { // можно сразу реструктурировать important из state
-            return {important: !important}
-        });
-    };
-
     render() {
-        const {label, onDeleted, onToggleImportant, onToggleDone} = this.props, // деструктурирование props потому что ф-я render() не принимает параметры
-              {done, important} = this.state;
+        const {label, onDeleted, onToggleImportant, onToggleDone, done, important} = this.props; // деструктурирование props потому что ф-я render() не принимает параметры
         console.log('done:', done, '\n', 'important:', important);
         let classNames = 'todo-list-item';
         classNames += done ? ' done' : '';
